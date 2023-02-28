03ca1e3afb84fee8c64bf9df48119ed3a48b3788
Pawel Sobaniec <infipablosoban@gmail.com>
2023-02-28 16:09:57 +0100

    styling cleanup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ["MontserratVariable", "Montserrat", ...defaultTheme.fontFamily.sans],
+				sans: ["OpenSansVariable", "OpenSans", ...defaultTheme.fontFamily.sans],
 			  },
 		},
 	},
