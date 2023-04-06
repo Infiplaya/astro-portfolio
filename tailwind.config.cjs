b746a3e8da0ce746c3ea219b3c1f38e5d1a4f7c1
Pawel Sobaniec <infipablosoban@gmail.com>
2023-04-06 19:29:31 +0200

    mobile homepage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,23 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
-  theme: {},
+  theme: {
+    extend: {
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+    },
+  },
   plugins: [require("@tailwindcss/typography")],
 };
